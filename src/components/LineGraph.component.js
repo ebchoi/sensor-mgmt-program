8a4c831f362f276019bb37a593655661b783d663
ebchoi <eunbichoi1@gmail.com>
2022-10-08 00:52:53 +0900

    Add: 그래프 최근데이터 불러오기

--- src/components/LineGraph.component.js
+++ src/components/LineGraph.component.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react';
+import { GET_GRAPH_RECENT_DATA_API } from '../config';
 import styled from 'styled-components';
-import React from 'react';
 
 function LineGraph() {
-  <Wrapper>Graph</Wrapper>;
+  const [recentFeeds, setRecentFeeds] = useState([]);
+
+  const getRecentData = async () => {
+    let url = `${GET_GRAPH_RECENT_DATA_API}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setRecentFeeds(data.feeds);
+  };
+
+  useEffect(() => {
+    getRecentData();
+  }, []);
+
+  return (
+    <Wrapper>
+      {recentFeeds.map(feed => {
+        return (
+          <div key={feed.entry_id}>
+            {feed.field1}, {feed.field2},{feed.field3}
+          </div>
+        );
+      })}
+    </Wrapper>
+  );
 }
 
 export default LineGraph;
--- src/pages/GraphDashboard.page.js
+++ src/pages/GraphDashboard.page.js
@@ -1,8 +1,11 @@
+import LineGraph from '../components/LineGraph.component';
+
 function GraphDashboard() {
   return (
     <>
       <h1>Graph</h1>
       <h2>화이팅🥊</h2>
+      <LineGraph />
     </>
   );
 }
