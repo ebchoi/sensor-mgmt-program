242614c5b985f44ca8a640d64966d32a435b53ec
Changhoon Oh <och.changhoon@gmail.com>
2022-10-08 10:18:43 +0900

    [ Modify ] SensorList 페이지 레이아웃 구현 v0.4

--- src/components/SensorTable.components.js
+++ src/components/SensorTable.components.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import React from 'react';
 
-export const SensorTable = () => {
+export function SensorTable() {
   <Wrapper>Table</Wrapper>;
-};
+}
 
 const Wrapper = styled.div``;
