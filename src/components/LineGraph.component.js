fe8c9a481524e0856852f5beaa305ee6c1b46fce
ebchoi <eunbichoi1@gmail.com>
2022-10-08 14:16:27 +0900

    Add: 피드데이터 입력날짜로 받아오기
    - 현재는 지정해준 정적인 날짜를 전달해서 받아오는 로직

--- src/components/LineGraph.component.js
+++ src/components/LineGraph.component.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
-import { GET_GRAPH_RECENT_DATA_API } from '../config';
 import styled from 'styled-components';
+import {
+  GET_GRAPH_RECENT_DATA_API,
+  GET_GRAPH_SELECTED_DATE_DATA_API,
+} from '../config';
 
 function LineGraph() {
   const [recentFeeds, setRecentFeeds] = useState([]);
+  const [requestedFeeds, setRequestedFeeds] = useState([]);
 
   const getRecentData = async () => {
     let url = `${GET_GRAPH_RECENT_DATA_API}`;
@@ -16,15 +20,49 @@ function LineGraph() {
     getRecentData();
   }, []);
 
+  const getSelectedDateData = async (startDate, endDate) => {
+    let url = `${GET_GRAPH_SELECTED_DATE_DATA_API}&start=${startDate}%2000:00:00&end=${endDate}%2000:00:00`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setRequestedFeeds(data.feeds);
+  };
+
   return (
     <Wrapper>
-      {recentFeeds.map(feed => {
-        return (
-          <div key={feed.entry_id}>
-            {feed.field1}, {feed.field2},{feed.field3}
-          </div>
-        );
-      })}
+      <button
+        onClick={() => {
+          getSelectedDateData('2022-07-01', '2022-07-02');
+        }}
+      >
+        get data for 2022-07-01
+      </button>
+      <FlexRow>
+        <Feeds>
+          <h1>최근 100개 데이터피드</h1>
+          {recentFeeds.map(feed => {
+            return (
+              <div key={feed.entry_id}>
+                <Field>{feed.field1}</Field>
+                <Field>{feed.field2}</Field>
+                <Field>{feed.field3}</Field>
+              </div>
+            );
+          })}
+        </Feeds>
+        <Feeds>
+          <h1>2022-07-01</h1>
+          {requestedFeeds &&
+            requestedFeeds.map(feed => {
+              return (
+                <div key={feed.entry_id}>
+                  <Field>{feed.field1}</Field>
+                  <Field>{feed.field2}</Field>
+                  <Field>{feed.field3}</Field>
+                </div>
+              );
+            })}
+        </Feeds>
+      </FlexRow>
     </Wrapper>
   );
 }
@@ -32,3 +70,17 @@ function LineGraph() {
 export default LineGraph;
 
 const Wrapper = styled.div``;
+const FlexRow = styled.div`
+  display: flex;
+`;
+
+const Feeds = styled.div`
+  border: 1px solid blue;
+  padding: 10px;
+  margin: 25px;
+`;
+
+const Field = styled.span`
+  padding: 5px;
+  margin: 0 5px;
+`;
